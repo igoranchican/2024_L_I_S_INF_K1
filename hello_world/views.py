b6f8cd1d2db5263f24bc031c4148553b9d850d8d
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-21 09:03:30 +0100

    lekcja 3

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,9 +2,11 @@ from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
-
 moje_imie = "Ihor"
+
+
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -13,6 +15,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
